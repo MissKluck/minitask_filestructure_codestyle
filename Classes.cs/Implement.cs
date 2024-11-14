7fff8698e7eec62e763728a76d1aaa1d969f5719
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-14 11:29:59 +0100

    implemented generic and igeneric to the class

--- Classes.cs/Implement.cs
+++ Classes.cs/Implement.cs
@@ -1,4 +1,4 @@
-public class Implement
+public class Implement<Gen1, Gen2> : Generic<Gen1, Gen2>, IGeneric
 {
     private readonly Generic<string, int> pokemon = new Generic<string, int>();
 
@@ -7,5 +7,15 @@ public class Implement
         pokemon.Type = "Fire";
         pokemon.Generation = 1;
     }
+
+    public void Display()
+    {
+        Console.WriteLine(ToString());
+    }
+
+    //public void DisplayMessage(string message)
+    //{
+    //    Console.WriteLine($"{message}");
+    //}
 
 }
