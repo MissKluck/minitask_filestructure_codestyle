28bca96d31835d6b2c55f88b5b1dfb395c023fae
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-14 11:28:50 +0100

    made the string nullable + updated tostring override

--- Classes.cs/Generic.cs
+++ Classes.cs/Generic.cs
@@ -3,11 +3,11 @@ public class Generic<T1, T2>
     public T1? Type { get; set; }
     public T2? Generation { get; set; }
     public int PokedexNumber { get; set; }
-    public string PokemonName { get; set; }
+    public string? PokemonName { get; set; }
 
     public override string ToString()
     {
-        return $"Your pokemon is {PokemonName} from generation {Generation}. It is a {Type} type pokemon, and it's Pokedex Number is {PokedexNumber}.";
+        return $"Your Pokemon is {PokemonName} from generation {Generation}. It is a Pokemon of type {Type}, and it's Pokedex number is {PokedexNumber}.";
     }
 
 }
