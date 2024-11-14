6be41e8403d61735e9310fceee12a7d6f5841d8f
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-14 10:00:14 +0100

    setup generic class

---
+++ Classes.cs/Generic.cs
@@ -0,0 +1,8 @@
+public class Generic<T1, T2>
+{
+    public T1? Type { get; set; }
+    public T2? Generation { get; set; }
+    public int PokedexNumber { get; set; }
+    public string PokemonName { get; set; }
+
+}
